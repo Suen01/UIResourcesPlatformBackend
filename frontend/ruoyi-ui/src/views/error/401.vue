4cf1e02c1cd9c0f9295222e2b1e228a4ba10a534
sgj <17600516722@163.com>
2022-05-21 10:16:31 +0800

    测试git2

--- frontend/ruoyi-ui/src/views/error/401.vue
+++ frontend/ruoyi-ui/src/views/error/401.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="errPage-container">
     <el-button icon="arrow-left" class="pan-back-btn" @click="back">
-      返回
+      返回1
     </el-button>
     <el-row>
       <el-col :span="12">
